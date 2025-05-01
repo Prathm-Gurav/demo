64924a1fe715f4e045fbe68b6b7dd4c47282f22f
Prathm-Gurav <PrathmeshGurav22@gmail.com>
2025-05-01 12:13:48 +0530

    wed

---
+++ src/main/java/Help/Help.java
@@ -0,0 +1,15 @@
+package Help;
+
+public class Help {
+
+//	1] First made only three games and then call by creating object --> Tight Coupling
+//	1] Used interface with the help of that we can call any game --> Lossely Coupling 1
+//	2] Made configuration class and Beans then called with context --> Lossely Coupling 2
+//
+//	3] Made helloWorld Package for learning Application Context, Configuration class and Beans
+//	4] Learned different types of calling beans and different types of declaring beans
+//	
+//	
+	
+	
+}
