1941f02b8dd3741b8b6d41c532fca79732f1c760
a <a@a-Vostro-3670>
2025-05-20 15:07:56 +0530

    wq

--- src/main/java/com/example/demo/GameRelated/App03GamingSpringBeans.java
+++ src/main/java/com/example/demo/GameRelated/App03GamingSpringBeans.java
@@ -1,14 +1,10 @@
 package com.example.demo.GameRelated;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
 
 import com.example.demo.game.GameRunner;
 import com.example.demo.game.GammingConsole;
-import com.example.demo.game.MarioGame;
-import com.example.demo.game.SuperContra;
 
-@Configuration
 public class App03GamingSpringBeans {
 	public static void main(String[] args) {
 		try(//1 Launch a Spring context
--- src/main/java/com/example/demo/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.demo.helloworld;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
